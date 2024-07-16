e3d03226b6bf9a514f929875b3e92a7d28166a33
Alessandro Leonardi <a.leonardi1995@icloud.com>
2024-07-16 10:50:25 -0300

    Initial Commit

---
+++ Virtual_Pet_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Virtual_Pet_App
+//
+//  Created by Alessandro Leonardi on 16/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
